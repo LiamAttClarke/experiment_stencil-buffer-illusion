b70e2daf891b89b95d2bb0ceeaf6929c57c6cac6
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2016-01-03 12:36:40 -0500

    Initial commit

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+    public GameObject exitPortal;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /*void OnTriggerEnter (Collider other) {
+        if (other.tag != "Portable") return;
+        Vector3 relativePos = other.transform.position - transform.position;
+        var clone = (GameObject)Instantiate(other.gameObject);
+        clone.transform.position = exitPortal.transform.position + relativePos;
+        
+    }
+
+    void OnTriggerExit (Collider other) {
+        if (other.tag != "Portable") return;
+
+    }*/
+}
